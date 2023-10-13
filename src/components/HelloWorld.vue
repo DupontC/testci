3e5a34966fe70a4a0f5f22cdf0c27c9ac6131ebe
Cyril DUPONT <cyril.dupont@ext.bpifrance.fr>
2023-10-13 15:20:12 +0200

    test deploy 2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,7 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>Vite + Vue</h1>
+  <h1>Vite + Vue 2</h1>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
